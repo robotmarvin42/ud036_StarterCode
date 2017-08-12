f182091f9062d41849c067ef9258852dc8ba23a3
Robot Marvin <lance.dofflemyer@gmail.com>
2017-08-12 19:09:27 -0400

    Add files via upload

--- media.py
+++ media.py
@@ -1,20 +1,29 @@
-#This class defines movies and their attributes
+# This class defines movies and their attributes
+
 import webbrowser
 from video import Video
 
+
+# Here is the Movie class!
 class Movie(Video):
+
     """This class provides a way to store movie related information"""
-    
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__ (self, title, VIEWER_RATING, length, comments, VALID_RATINGS, movie_storyline, poster_image, trailer_youtube): 
+    # Initiate class here
+    def __init__(self, title, VIEWER_RATING, length, comments,
+                 VALID_RATINGS, movie_storyline, poster_image,
+                 trailer_youtube):
+        # Pulling in the Video class here
         Video.__init__(self, title, VIEWER_RATING, length, comments)
+
+        # Creating additional attributes here for
+        # Movie class
         self.movie_rating = VALID_RATINGS
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
+    # Method that calls the webbrowser to open
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-    
-
--- video.py
+++ video.py
@@ -1,13 +1,14 @@
-#Class for video content
-#This class provides variable for the movie class, and demonstrates nested classes.
+# Class for video content
+# This class provides variable for the movie class,
+# and demonstrates nested classes.
+
 
 class Video():
     """The base class for video records"""
 
     # Class Variables
-    VIEWER_RATING = [1, 2, 3, 4, 5] #1 = Hate it. 5 = Love it.
+    VIEWER_RATING = [1, 2, 3, 4, 5]  # 1 = Hate it. 5 = Love it.
 
-   
     # init definition with base, unique attributes
     def __init__(self, title, VIEWER_RATING, length, comments):
         self.title = title
@@ -15,8 +16,7 @@ class Video():
         self.length = length
         self.comments = comments
 
-
-    # Append comments to comment field. Functionality to be added to website at later time.
+    # Append comments to comment field. Functionality to be
+    # added to website at later time.
     def add(self, comment):
         self.comments.append(comment)
-
